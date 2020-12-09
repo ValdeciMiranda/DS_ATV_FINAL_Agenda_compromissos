3b804a89a6cb8e0485b9cc7dd949dd4fa3537d20
VALDECI MIRANDA <valdeci.miranda@etec.sp.gov.br>
2020-12-09 18:05:48 -0300

    Segunda atualização

---
+++ DAL_DS_ATV_FINAL_Agenda_compromissos/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_DS_ATV_FINAL_Agenda_compromissos
+{
+    public class Class1
+    {
+    }
+}
